c1b5e364f3fb9e128e8c42e0e742d6c5fc5181b3
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2016-05-15 01:20:11 +0200

    Rename Reference to Data

---
+++ test/data.test.js
@@ -0,0 +1,16 @@
+import expect from 'expect.js'
+import Data from '../lib/data'
+
+
+
+describe('Reference', function () {
+
+    it('should set data', function () {
+        const object = {}
+        const key = 'hello'
+
+        Data.set(object, key, 'world')
+        expect(Data.get(object, key)).to.be.eql('world')
+    })
+
+})
