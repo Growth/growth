eb3ec76a596446b855b3b5c5414af2b7a122fe72
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2016-06-28 23:55:14 +0200

    Move to 1.2.0

--- test/tag.test.js
+++ test/tag.test.js
@@ -12,109 +12,60 @@ describe('Tag', function () {
 
 
     it('should tag an item', function () {
-        Tag.set(item, 'tagA')
-        expect(Tag.has(item, 'tagA')).to.be.ok()
+        Tag.tag(item, 'tagA')
+        expect(Tag.hasTag(item, 'tagA')).to.be.ok()
 
-        const tagB = Tag.get('tagB')
-        Tag.set(item, tagB)
-        expect(Tag.has(item, tagB)).to.be.ok()
+        const tagB = Tag.getTag('tagB')
+        Tag.tag(item, tagB)
+        expect(Tag.hasTag(item, tagB)).to.be.ok()
     })
 
 
 
     it('should untag an item', function () {
-        Tag.unset(item, 'tagC')
-        expect(Tag.has(item, 'tagC')).to.not.be.ok()
+        Tag.untag(item, 'tagC')
+        expect(Tag.hasTag(item, 'tagC')).to.not.be.ok()
     })
 
 
 
     it('should check if a tag has an item', function () {
-        Tag.set(item, 'tagD')
-        expect(Tag.has(item, 'tagD')).to.be.ok()
+        Tag.tag(item, 'tagD')
+        expect(Tag.hasTag(item, 'tagD')).to.be.ok()
     })
 
 
 
     it('should get a tag', function () {
-        countries.forEach((country) => Tag.set(country, 'tagD'))
+        countries.forEach((country) => Tag.tag(country, 'tagD'))
 
-        expect(Tag.get('country')).to.be.eql(new Set(countries))
+        expect(Tag.getTag('country')).to.be.eql(new Set(countries))
     })
 
 
 
     it('should clear a tag', function () {
-        countries.forEach((country) => Tag.set(country, 'tagE'))
-        Tag.clear('tagE')
+        countries.forEach((country) => Tag.tag(country, 'tagE'))
+        Tag.clearTag('tagE')
 
-        expect(Tag.get('tagE').size).to.be.eql(0)
+        expect(Tag.getTag('tagE').size).to.be.eql(0)
     })
 
 
 
     it('should delete a tag', function () {
-        Tag.set(item, 'tagF')
-        const tagF = Tag.get('tagF')
-        Tag.destroy('tagF')
+        Tag.tag(item, 'tagF')
+        const tagF = Tag.getTag('tagF')
+        Tag.destroyTag('tagF')
 
-        expect(Tag.get('tagF') !== tagF).to.be.ok()
+        expect(Tag.getTag('tagF') !== tagF).to.be.ok()
     })
 
 
 
     it('should get tag by name or by the tag itself', function () {
-        const tagG = Tag.get('tagG')
-        expect(Tag.get('tagG') === Tag.get(tagG)).to.be.ok()
-    })
-
-
-
-    it('should get a tag with namespace', function () {
-        const CustomTag = Tag.namespace('customA')
-        Tag.set(item, CustomTag.get('tagH'))
-        expect(Tag.has(item, 'customA.tagH')).to.be.ok()
-    })
-
-
-
-    it('should check if a tag exists with namespace', function () {
-        const CustomTag = Tag.namespace('customB')
-        expect(CustomTag.exists('tagI')).to.not.be.ok()
-        Tag.create('customB.tagI')
-        expect(CustomTag.exists('tagI')).to.be.ok()
-    })
-
-
-
-    it('should create a tag with namespace', function () {
-        const CustomTag = Tag.namespace('customC')
-        expect(CustomTag.create('tagJ')).to.be.ok()
-        expect(CustomTag.create('tagJ')).to.not.be.ok()
-    })
-
-
-
-    it('should tag an item with namespace', function () {
-        const CustomTag = Tag.namespace('customD')
-        CustomTag.set(item, 'tagH')
-        expect(CustomTag.has(item, 'tagH')).to.be.ok()
-    })
-
-
-
-    it('should untag an item with namespace', function () {
-        const CustomTag = Tag.namespace('customE')
-        CustomTag.unset(item, 'tagI')
-        expect(CustomTag.has(item, 'tagI')).to.not.be.ok()
-    })
-
-
-
-    it('should check if a tag has an item with namespace', function () {
-        const CustomTag = Tag.namespace('customF')
-        CustomTag.set(item, 'tagJ')
-        expect(CustomTag.has(item, 'tagJ')).to.be.ok()
+        const tagG = Tag.getTag('tagG')
+        expect(Tag.getTag('tagG') === Tag.getTag(tagG)).to.be.ok()
     })
 
 
--- test/utils.test.js
+++ test/utils.test.js
@@ -1,18 +1,5 @@
 import expect from 'expect.js'
-import {
-    delayApply,
-    delayCall,
-    delay,
-    wrap,
-    check,
-    isString,
-    isFunction,
-    isUndefined,
-    addNamespace,
-    stringToArray,
-    remove,
-    spawnTask
-} from '../lib/utils'
+import Utils from '../lib/utils'
 
 
 
@@ -27,12 +14,12 @@ describe('Utils', function () {
 
 
 
-    it('should delay', delay)
+    it('should delay', Utils.delay)
 
 
 
     it('should delayed apply', function (done) {
-        delayApply(function (arg1, arg2) {
+        Utils.delayApply(function (arg1, arg2) {
             expect(arg1 && arg2).to.be.ok()
             done()
         }, null, [true, true])
@@ -41,7 +28,7 @@ describe('Utils', function () {
 
 
     it('should delayed call', function (done) {
-        delayCall(function (arg1, arg2) {
+        Utils.delayCall(function (arg1, arg2) {
             expect(arg1 && arg2).to.be.ok()
             done()
         }, null, true, true)
@@ -50,27 +37,27 @@ describe('Utils', function () {
 
 
     it('should check types', function () {
-        expect(check('Hello', 'string')).to.be.ok()
-        expect(check({}, 'object')).to.be.ok()
+        expect(Utils.check('Hello', 'string')).to.be.ok()
+        expect(Utils.check({}, 'object')).to.be.ok()
     })
 
 
 
     it('should check string type', function () {
-        expect(isString('Hello')).to.be.ok()
+        expect(Utils.isString('Hello')).to.be.ok()
     })
 
 
 
     it('should check undefined type', function () {
         let hello
-        expect(isUndefined(hello)).to.be.ok()
+        expect(Utils.isUndefined(hello)).to.be.ok()
     })
 
 
 
     it('should check function type', function () {
-        expect(isFunction(function() {})).to.be.ok()
+        expect(Utils.isFunction(function() {})).to.be.ok()
     })
 
 
@@ -79,7 +66,7 @@ describe('Utils', function () {
         function hello (a, b) {
             return a + b
         }
-        const wrappedFunction = wrap(hello, function (a, b) {
+        const wrappedFunction = Utils.wrap(hello, function (a, b) {
             a += 1
             b += 1
             return [a, b]
@@ -89,23 +76,9 @@ describe('Utils', function () {
 
 
 
-    it('should prefix a string with namespace', function () {
-        const namespaced = addNamespace('identifier', 'namespace')
-        expect(namespaced).to.be.eql('namespace.identifier')
-    })
-
-
-
-    it('should should convert a string to array', function () {
-        expect(stringToArray('hello, world')).to.be.eql(['hello', 'world'])
-        expect(stringToArray('hello; world', ';')).to.be.eql(['hello', 'world'])
-    })
-
-
-
     it('should remove an item from an array', function () {
         const array = [1, 2, 3]
-        remove(array, 3)
+        Utils.remove(array, 3)
         expect(array).to.be.eql([1, 2])
     })
 
@@ -118,7 +91,7 @@ describe('Utils', function () {
     }
 
     it('should spawn a task', function (done) {
-        spawnTask(function * (next) {
+        Utils.spawnTask(function * (next) {
             let passed
 
             passed = yield asyncFunction(next, true)
